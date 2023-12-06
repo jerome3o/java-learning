53bebb2d1a382afff6db54ecd68722ea1bbf6fd9
Jerome Swannack <jeromeswannack@gmail.com>
2023-12-06 17:42:13 +1300

    Initial gradle project

---
+++ src/MyClass.java
@@ -0,0 +1,7 @@
+import utils.wtf.MyUtils;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println(MyUtils.capitalize("hello World! hey"));
+    }
+}
---
+++ src/utils/MyUtils.java
@@ -0,0 +1,10 @@
+package utils.wtf;
+
+public class MyUtils {
+
+    public static String capitalize(String str) {
+        // capitalize string
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
+}
